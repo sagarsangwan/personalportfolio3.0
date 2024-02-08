5427a4100200771a98163f5d9434225ca7143860
sagar sangwan <iamsagarsangwan@gmail.com>
2024-02-09 01:05:34 +0530

    added about me section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import Aboutme from "@/components/aboutme";
 export default function Home() {
   return (
-    <main className="container border-r-2  h-screen">
+    <main className="container border-r-2 height-89vh md:h-screen">
       <Navbar />
       <Hero />
+      <Aboutme />
     </main>
   );
 }
